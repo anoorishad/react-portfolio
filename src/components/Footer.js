78843f34b380f3506449a1028cc4056f6fd2ae93
anoorishad <anoorishad@me.com>
2022-06-08 19:00:02 -0700

    initial structure

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer class="footer mt-auto py-3 bg-light">
+            <div class="container">
+                <span class="text-muted">Place sticky footer content here.</span>
+            </div>
+        </footer>
+        // <div>
+        // <h1>Footer</h1>
+        // </div>
+    );
+}

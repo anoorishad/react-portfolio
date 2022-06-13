34f7bc202da5a66ba8f39c72d5b675a288d3159b
anoorishad <anoorishad@me.com>
2022-06-13 11:19:38 -0700

    heroku deploy

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,18 +5,13 @@ import './About.css'
 export default function About() {
   const headShot = {
     image: Headshot
-}
+  }
   return (
     <div className='bodyStyle'>
       <h1>About</h1>
-      <img className='headShot' src={headShot.image} alt='head shot'/>
+      <img className='headShot' src={headShot.image} alt='head shot' />
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Full-stack web developer leveraging a background in IT, looking to balance functionality and design while optimizing the end-user experience. Recently earned a certificate in full-stack development from the University of Washington, with skills in JavaScript, CSS, React.js, and responsive web design. I approach every project analytically and am determined to produce results I take pride in. I’m excited to apply my skills in a fast-paced and adaptable team, driven to create unique web experiences.
       </p>
     </div>
   );

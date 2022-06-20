106b85870b748a717c20acaa493525950fc7af78
anoorishad <anoorishad@me.com>
2022-06-19 22:38:17 -0700

    added about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './About.css'
+import ME from '../../assets/meWithBg.jpeg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
     return (
-        <div>About</div>
+        <section id='about'>
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt="About Image" />
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className='about__card'>
+                            <FaAward className='about__icont'/>
+                            <h5>Experience</h5>
+                            <small>Coding Bootcamp Graduate</small>
+                        </article>
+                        <article className='about__card'>
+                            <FiUsers className='about__icont'/>
+                            <h5>Clients</h5>
+                            <small>5+ Worldwide</small>
+                        </article>
+                        <article className='about__card'>
+                            <VscFolderLibrary className='about__icont'/>
+                            <h5>Projects</h5>
+                            <small>5+ Completed</small>
+                        </article>
+                    </div>
+
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, dicta. Tenetur minus fuga suscipit commodi facilis assumenda alias laboriosam iste deserunt architecto temporibus, fugit eum qui, perspiciatis optio odit dolorem.
+                    </p>
+
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-            <a href="https://github.com" target="_blank"><BsGithub/></a>
+            <a href="https://linkedin.com/in/anoorishad" target="_blank"><BsLinkedin/></a>
+            <a href="https://github.com/anoorishad" target="_blank"><BsGithub/></a>
         </div>
     )
 }

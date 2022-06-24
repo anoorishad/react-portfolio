8b3479cb42ad9eb61ff4b0c0844596826055e549
anoorishad <anoorishad@me.com>
2022-06-23 17:00:58 -0700

    completed contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_zreqcko', 'template_3uqueug', form.current, 'g4lYyVgdwOVKj9zAd')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+            e.target.reset()
+    };
+
+
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -13,30 +31,30 @@ const Contact = () => {
             <div className="container contact__container">
                 <div className="contact__options">
                     <article className='contact__option'>
-                        <MdOutlineEmail className='contact__option-icon'/>
+                        <MdOutlineEmail className='contact__option-icon' />
                         <h4>Email</h4>
                         <h5>anoorishad@me.com</h5>
                         <a href="mailto:anoorishad@me.com" target='_blank'>Send a message</a>
                     </article>
                     <article className='contact__option'>
-                        <RiMessengerLine className='contact__option-icon'/>
+                        <RiMessengerLine className='contact__option-icon' />
                         <h4>Facebook Messenger</h4>
                         <h5>anoorishad</h5>
                         <a href="https://m.me/anoorishad" target='_blank'>Send a message</a>
                     </article>
                     <article className='contact__option'>
-                        <BsWhatsapp className='contact__option-icon'/>
+                        <BsWhatsapp className='contact__option-icon' />
                         <h4>WhatsApp</h4>
-                        <h5>+123456</h5>
-                        <a href="https://api.whatsapp.com/send?phone=+123456" target='_blank'>Send a message</a>
+                        <h5>+12067158989</h5>
+                        <a href="https://api.whatsapp.com/send?phone=+12067158989" target='_blank'>Send a message</a>
                     </article>
                 </div>
                 {/* END OF CONTACT OPTIONS */}
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name='name' placeholder='Your Full Name' required />
                     <input type="email" name='email' placeholder='Your Email' required />
                     <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-                    <button type='submin' className='btn btn-primary'>Send Message</button>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
                 </form>
             </div>
         </section>
